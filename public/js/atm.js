ed857aaa2d1b6b17e650a889e094b8922b0233aa
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2017-09-12 15:57:05 -0300

    Communication with API server. Modal dialogs

--- public/js/atm.js
+++ public/js/atm.js
@@ -1,6 +1,6 @@
 var atmApp = angular.module("atmApp", []);
 
-atmApp.controller("atmController", ["$scope", "$http", function($scope, $http) {
+atmApp.controller("atmController", ["$scope", "$http", "$location", function($scope, $http, $location) {
 
     var currentState = "menu";
 
@@ -13,6 +13,7 @@ atmApp.controller("atmController", ["$scope", "$http", function($scope, $http) {
     }
 
     $scope.showDepositForm = function() {
+        resetDepositForm();
         currentState = "deposit";
     }
 
@@ -20,12 +21,107 @@ atmApp.controller("atmController", ["$scope", "$http", function($scope, $http) {
         currentState = "withdraw";
     }
 
-    $scope.deposit = function(form) {
-        console.log("deposit %s", JSON.stringify(form));
+    resetDepositForm = function() {
+        $scope.amountBills = [
+            { value: 2, quantity: 0 },
+            { value: 5, quantity: 0 },
+            { value: 10, quantity: 0 },
+            { value: 20, quantity: 0 },
+            { value: 50, quantity: 0 },
+            { value: 100, quantity: 0 }
+        ]
     }
 
+    $scope.increaseAmount = function(bill) {
+        bill.quantity++;
+    }
+
+    $scope.decreaseAmount = function(bill) {
+        if (bill.quantity > 0) {
+            bill.quantity--;
+        }
+    }
+    /*
+     * Make a deposit
+     */
+    $scope.deposit = function(form) {
+        console.log("deposit %s", JSON.stringify(form));
+
+        var data = $scope.amountBills;
+
+        httpRequest("/account/" + form.accountNumber + "/deposit", data, function(response) {
+            showDepositModal(response.data);
+        });
+    }
+
+    showDepositModal = function(data) {
+        $("#deposit-modal").modal("show");
+        console.log(data);
+        $scope.depositModalMessage = data;
+    }
+
+    /* 
+     * Make a withdraw
+     */
     $scope.withdraw = function(form) {
         console.log("withdraw %s", JSON.stringify(form));
+
+        var data = {
+            amount: form.amount
+        };
+
+        httpRequest("/account/" + form.accountNumber + "/withdrawal", data, function(response) {
+            showWithdrawalModal(response.data);
+        });
+    }
+
+    /*
+     * Show withdrawal success modal
+     */
+    showWithdrawalModal = function(data) {
+        $("#withdraw-modal").modal("show");
+        $scope.withdrawalModalMessage = data;
+    }
+
+    $scope.hasErrorMessage = function() {
+        return $scope.errorMessage;
+    }
+
+    $scope.dismissAlert = function() {
+        $scope.errorMessage = null;
+    }
+
+    /*
+     * Display generic error message
+     */
+    showGeneralErrorMessage = function(message) {
+        $scope.errorMessage = message;
+    }
+
+    /*
+     * Handles HTTP requests
+     */
+    var httpRequest = function(service, data, successCallback, errorCallback) {
+        $scope.dismissAlert();
+        var httpServer = $location.protocol() + "://" + $location.host() + ":9000";
+
+        $http({
+            method: "POST",
+            url: httpServer + service, 
+            data: data 
+        }).then( function (response) {
+            if (successCallback) {
+                successCallback(response);
+            }
+        }, function (response) {
+            if (!errorCallback || !errorCallback(response)) {
+                if (response.data.exception) {
+                    showGeneralErrorMessage(response.data.exception);
+                } else {
+                    showGeneralErrorMessage("Problemas ao acessar servidor. Tente mais tarde.");      
+                }
+            }
+        });
     }
 
 }]);
